3b936e605f7b29869c5cfd055eb36920b2892456
Al135663 <d3953514@live.tees.ac.uk>
2025-03-28 09:39:13 +0000

    final changes

--- Main.py
+++ Main.py
@@ -1,96 +1,115 @@
 from pyamaze import maze, agent, COLOR
-from collections import deque
 from queue import PriorityQueue
 import math
+import time
 
-start_point = (8, 10)  # (row, col) - Change this to any valid cell
-goal_point = (3, 3)  # (row, col) - Change this to any valid cell
+start_point = (13, 17)  # (row, col)
+goal_point = (3, 2)     # (row, col)
 
-def h(cell1, cell2):
+def H(cell1, cell2):
     """ Heuristic function (Manhattan Distance) """
     x1, y1 = cell1
     x2, y2 = cell2
     return abs(x1 - x2) + abs(y1 - y2)
 
-####################################
-
-""" Heuristic function (Euclidean Distance) """
-def e(cell1, cell2):
+def E(cell1, cell2):
+    """ Heuristic function (Euclidean Distance) """
     x1, y1 = cell1
     x2, y2 = cell2
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
-####################################
+R = E  # Change to H for Manhattan, E for Euclidean Distance
 
-R=e   # Change this to e for Euclidean Distance or h for Manhattan Distance.
-
-####################################  
 def BFS(m):
     """ Breadth-First Search (FIFO) """
     start = start_point  
     frontier = [start]
-    explored = [start]
+    explored = {start}
     bfsPath = {}
 
+    a = agent(m, start[0], start[1], footprints=True, color=COLOR.red, shape='square')
+    
     while frontier:
-        currCell = frontier.pop(0)  
+        currCell = frontier.pop(0)
+        m.tracePath({a: [currCell]}, delay=100)  # Update agent's position
+        time.sleep(0.1)  # Visualization delay
         if currCell == goal_point:
             break
+        
         for d in 'ESNW':
             if m.maze_map[currCell][d]:
-                childCell = (currCell[0] + (d == 'S') - (d == 'N'), 
-                             currCell[1] + (d == 'E') - (d == 'W'))
+                if d == 'E':
+                    childCell = (currCell[0], currCell[1] + 1)
+                elif d == 'W':
+                    childCell = (currCell[0], currCell[1] - 1)
+                elif d == 'S':
+                    childCell = (currCell[0] + 1, currCell[1])
+                else:
+                    childCell = (currCell[0] - 1, currCell[1])
                 
-                if childCell in explored:
-                    continue
-                
-                explored.append(childCell)
-                frontier.append(childCell)
-                bfsPath[childCell] = currCell
+                if childCell not in explored:
+                    explored.add(childCell)
+                    frontier.append(childCell)
+                    bfsPath[childCell] = currCell
 
-    fwdPath = {}
-    cell = goal_point
-    steps = 0
-    while cell != start:
-        fwdPath[bfsPath[cell]] = cell
-        cell = bfsPath[cell]
-        steps += 1
-    return fwdPath, steps
+    # Reconstruct path
+    path = []
+    if currCell == goal_point:
+        cell = goal_point
+        while cell != start:
+            path.append(cell)
+            cell = bfsPath[cell]
+        path.reverse()
+    return path, len(path)
 
-####################################  
+
+####################################################
+
+
 def DFS(m):
     """ Depth-First Search (LIFO) """
     start = start_point  
     explored = [start]
     frontier = [start]
     dfsPath = {}
+    
+    a = agent(m, start[0], start[1], footprints=True, color=COLOR.blue, shape='square')
 
     while frontier:
-        currCell = frontier.pop()  
+        currCell = frontier.pop()
+        m.tracePath({a: [currCell]}, delay=100)  # Update agent's position
+        time.sleep(0.1)  # Visualization delay
         if currCell == goal_point:
             break
+        
         for d in 'ESNW':
             if m.maze_map[currCell][d]:
-                childCell = (currCell[0] + (d == 'S') - (d == 'N'), 
-                             currCell[1] + (d == 'E') - (d == 'W'))
+                if d == 'E':
+                    childCell = (currCell[0], currCell[1] + 1)
+                elif d == 'W':
+                    childCell = (currCell[0], currCell[1] - 1)
+                elif d == 'S':
+                    childCell = (currCell[0] + 1, currCell[1])
+                else:
+                    childCell = (currCell[0] - 1, currCell[1])
                 
-                if childCell in explored:
-                    continue
-                
-                explored.append(childCell)
-                frontier.append(childCell)    
-                dfsPath[childCell] = currCell
+                if childCell not in explored:
+                    explored.append(childCell)
+                    frontier.append(childCell)    
+                    dfsPath[childCell] = currCell
 
-    fwdPath = {}
-    cell = goal_point
-    steps = 0
-    while cell != start:
-        fwdPath[dfsPath[cell]] = cell
-        cell = dfsPath[cell]
-        steps += 1
-    return fwdPath, steps
+    # Reconstruct path
+    path = []
+    if currCell == goal_point:
+        cell = goal_point
+        while cell != start:
+            path.append(cell)
+            cell = dfsPath[cell]
+        path.reverse()
+    return path, len(path)
 
-####################################  
+####################################################
+
 def aStar(m):
     """ A* Search Algorithm """
     start = start_point  
@@ -101,17 +120,27 @@ def aStar(m):
 
     open_set = PriorityQueue()
     open_set.put((f_score[start], start))
-    
     aPath = {}
+    
+    a = agent(m, start[0], start[1], footprints=True, color=COLOR.green, shape='square')
 
     while not open_set.empty():
         currCell = open_set.get()[1]
+        m.tracePath({a: [currCell]}, delay=100)  # Update agent's position
+        time.sleep(0.1)  # Visualization delay
         if currCell == goal_point:
             break
+        
         for d in 'ESNW':
             if m.maze_map[currCell][d]:
-                childCell = (currCell[0] + (d == 'S') - (d == 'N'), 
-                             currCell[1] + (d == 'E') - (d == 'W'))
+                if d == 'E':
+                    childCell = (currCell[0], currCell[1] + 1)
+                elif d == 'W':
+                    childCell = (currCell[0], currCell[1] - 1)
+                elif d == 'S':
+                    childCell = (currCell[0] + 1, currCell[1])
+                else:
+                    childCell = (currCell[0] - 1, currCell[1])
                 
                 temp_g_score = g_score[currCell] + 1
                 temp_f_score = temp_g_score + R(childCell, goal_point)
@@ -122,38 +151,50 @@ def aStar(m):
                     open_set.put((temp_f_score, childCell))
                     aPath[childCell] = currCell
 
-    fwdPath = {}
-    cell = goal_point
-    steps = 0
-    while cell != start:
-        fwdPath[aPath[cell]] = cell
-        cell = aPath[cell]
-        steps += 1
-    return fwdPath, steps
+    # Reconstruct path
+    path = []
+    if currCell == goal_point:
+        cell = goal_point
+        while cell != start:
+            path.append(cell)
+            cell = aPath[cell]
+        path.reverse()
+    return path, len(path)
 
-####################################  
+def traceFinalPath(m, agent, path):
+    """ Trace the final path from start to goal """
+    if not path:
+        return
+    # Move agent to the start position first
+    m.tracePath({agent: [path[0]]}, delay=100, kill=False)
+    # Trace each step of the path
+    for i in range(1, len(path)):
+        m.tracePath({agent: [path[i]]}, delay=100, kill=False)
+        time.sleep(0.1)
+
+#####################################################
+
 if __name__ == "__main__":
-    m = maze(10, 15)
-    m.CreateMaze(goal_point[0], goal_point[1], loopPercent=50, theme=COLOR.dark)  
+    m = maze(15, 20)
+    m.CreateMaze(goal_point[0], goal_point[1], loopPercent=100, theme=COLOR.dark)
 
-    # Generate paths and count steps for each algorithm
+    # Generate paths
     path_BFS, steps_BFS = BFS(m)
     path_DFS, steps_DFS = DFS(m)
     path_AStar, steps_AStar = aStar(m)
 
-    # Print steps taken by each agent
-    print(f"BFS Steps: {steps_BFS}")
-    print(f"DFS Steps: {steps_DFS}")
-    print(f"A* Steps: {steps_AStar}")
+    print(f"BFS Path Length: {steps_BFS}")
+    print(f"DFS Path Length: {steps_DFS}")
+    print(f"A* Path Length: {steps_AStar}")
 
-    # Create three agents with different colors
-    agent_BFS = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.red)      
-    agent_DFS = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.blue)     
-    agent_AStar = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.green)  
+    # Create agents for final paths with custom shapes
+    agent_BFS = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.yellow, shape='arrow')
+    agent_DFS = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.black, shape='triangle')
+    agent_AStar = agent(m, start_point[0], start_point[1], footprints=True, color=COLOR.cyan, shape='classic')
 
-    # Trace the paths for each agent
-    m.tracePath({agent_BFS: path_BFS})
-    m.tracePath({agent_DFS: path_DFS})
-    m.tracePath({agent_AStar: path_AStar})
+    # Trace final paths
+    traceFinalPath(m, agent_BFS, path_BFS)
+    traceFinalPath(m, agent_DFS, path_DFS)
+    traceFinalPath(m, agent_AStar, path_AStar)
 
     m.run()
